259272032a583e40e9ca517c1bfd6489d7e2cd6a
TSharma95 <102057323+TSharma95@users.noreply.github.com>
2023-03-04 11:51:04 +0530

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { Box } from '@mui/material';
 
 import { ChannelDetail, VideoDetail, SearchFeed, Navbar, Feed } from './components';
 
 const App = () => (
-  <BrowserRouter>
+  <HashRouter>
     <Box sx={{ backgroundColor: '#000' }}>
       <Navbar />
       <Routes>
@@ -14,7 +14,7 @@ const App = () => (
         <Route path='/search/:searchTerm' element={<SearchFeed />} />
       </Routes>
     </Box>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 export default App;
